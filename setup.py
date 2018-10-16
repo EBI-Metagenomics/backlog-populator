66566ae4e41d2ac73aa8d24446fcabf5f7a2abf7
Miguel Boland <mdb@ebi.ac.uk>
2018-10-16 16:42:06 +0100

    Fixed depdency installation

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import os
+
+_base = os.path.dirname(os.path.abspath(__file__))
+_requirements = os.path.join(_base, 'requirements.txt')
+
+with open(_requirements) as f:
+    install_requirements = f.read().splitlines()
 
 setup(name='emg-backlog-populator',
       version='1.0.2',
@@ -9,7 +16,8 @@ setup(name='emg-backlog-populator',
       packages=find_packages(),
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
-      requires=['dateutil', 'PyYAML', 'django', 'backlog'],
+      install_requires=install_requirements,
+      dependency_links=['http://github.com/EBI-Metagenomics/emg-backlog-schema.git#egg=emg-backlog-schema'],
       entry_points={
           'console_scripts': [
               'backlog_populator=backlog_populator.update:main'
