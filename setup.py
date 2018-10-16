13c9cb907ef00f4aa0adc607b0e477207c093d69
Miguel Boland <mdb@ebi.ac.uk>
2018-10-16 16:43:13 +0100

    Fixed dependency url

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import os
-
 _base = os.path.dirname(os.path.abspath(__file__))
 _requirements = os.path.join(_base, 'requirements.txt')
 
 with open(_requirements) as f:
     install_requirements = f.read().splitlines()
+
 
 setup(name='emg-backlog-populator',
       version='1.0.2',
@@ -17,7 +17,7 @@ setup(name='emg-backlog-populator',
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       install_requires=install_requirements,
-      dependency_links=['http://github.com/EBI-Metagenomics/emg-backlog-schema.git#egg=emg-backlog-schema'],
+      dependency_links = ['http://github.com/EBI-Metagenomics/emg-backlog-schema.git#egg=emg-backlog-schema'],
       entry_points={
           'console_scripts': [
               'backlog_populator=backlog_populator.update:main'
